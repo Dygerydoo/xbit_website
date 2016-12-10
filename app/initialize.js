cb084f809c75ee750b669267b38dac8538ab3e46
Francisco Casero <caserofrancisco@gmail.com>
2016-12-10 13:55:51 +0100

    add dropdown ability to menu navigation

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-menu]').onclick = function(){
-      console.log('CLICKED');
+  document.querySelector('[data-menu]').onclick = function(){
+      document.getElementsByClassName("example color");
   };
+
+  var menu_list = document.querySelector('.xbt-NavigationList');
+
+  document.querySelector('[data-menu]').addEventListener('click', function(e) {
+              e.preventDefault();
+              menu_list.classList.toggle('active');
+  });
 })
